ba2941cfcaafa64166ffc068b23a70624e256de3
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-10-05 15:02:08 +0500

    update images_service_test.go

--- images/images_service_test.go
+++ images/images_service_test.go
@@ -3,28 +3,19 @@ package images
 import (
 	"errors"
 	"fmt"
+	"image"
 	"os"
 	"testing"
 )
 
 var imagesService = ImagesService{}
 
-var img []byte
-
-func TestMain(m *testing.M) {
-	bytes, readErr := os.ReadFile("./mock/street.jpg")
-	if readErr != nil {
-		fmt.Println("failed to read image file")
-		os.Exit(1)
-	}
-
-	img = bytes
-	os.Exit(m.Run())
-}
+var img image.Image
+var imgBytes []byte
 
 func TestImagesService_CheckIsValidImage(t *testing.T) {
 	t.Run("valid image", func(t *testing.T) {
-		valid := imagesService.CheckIsValidImage(img)
+		valid := imagesService.CheckIsValidImage(imgBytes)
 		if !valid {
 			t.Fatalf("expected image to be valid")
 		}
@@ -47,28 +38,46 @@ func TestImagesService_Blur(t *testing.T) {
 	})
 
 	t.Run("should return InvalidBlurPercentageError if percentage < 0 or > 100 ", func(t *testing.T) {
-		_, err := imagesService.Blur(img, -1)
+		_, err := imagesService.Blur(imgBytes, -1)
 		if !errors.Is(err, InvalidBlurPercentageError) {
 			t.Fatalf("expected error to be InvalidBlurPercentageError")
 		}
 
-		_, err = imagesService.Blur(img, 101)
+		_, err = imagesService.Blur(imgBytes, 101)
 		if !errors.Is(err, InvalidBlurPercentageError) {
 			t.Fatalf("expected error to be InvalidBlurPercentageError")
 		}
 	})
 
 	t.Run("should return blurred image", func(t *testing.T) {
-		blurredImg, err := imagesService.Blur(img, 50)
+		blurredImgBytes, err := imagesService.Blur(imgBytes, 0)
 		if err != nil {
 			t.Fatalf("expected no error, got %v", err)
 		}
-
-		if len(*blurredImg) == 0 {
+		if len(*blurredImgBytes) == 0 {
 			t.Fatalf("expected blurred image to have bytes")
 		}
 
-		// Uncomment the following line to save the blurred image
-		//os.WriteFile("./mock/street_blurred.jpg", *blurredImg, 0644)
+		// Uncomment the following line to save then compare the blurred image
+		//os.WriteFile("./mock/street_blurred.jpg", *blurredImgBytes, 0644)
 	})
+}
+
+func TestMain(m *testing.M) {
+	bytes, readErr := os.ReadFile("./mock/street.jpg")
+	if readErr != nil {
+		fmt.Println("failed to read image file")
+		os.Exit(1)
+	}
+
+	origImg, err := imagesService.bytesToImage(bytes)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	imgBytes = bytes
+	img = *origImg
+
+	os.Exit(m.Run())
 }
