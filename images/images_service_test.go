f820be43c990297e4576f939f8214f88baf49003
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-10-05 18:41:10 +0500

    update images_service_test.go

--- images/images_service_test.go
+++ images/images_service_test.go
@@ -3,17 +3,19 @@ package images
 import (
 	"errors"
 	"fmt"
+	"image"
 	"os"
 	"testing"
 )
 
 var imagesService = ImagesService{}
 
-var img []byte
+var imgBytes []byte
+var img image.Image
 
 func TestImagesService_CheckIsValidImage(t *testing.T) {
 	t.Run("valid image", func(t *testing.T) {
-		valid := imagesService.CheckIsValidImage(img)
+		valid := imagesService.CheckIsValidImage(imgBytes)
 		if !valid {
 			t.Fatalf("expected image to be valid")
 		}
@@ -36,19 +38,20 @@ func TestImagesService_Blur(t *testing.T) {
 	})
 
 	t.Run("should return InvalidBlurPercentageError if percentage < 0 or > 100 ", func(t *testing.T) {
-		_, err := imagesService.Blur(img, -1)
+		_, err := imagesService.Blur(imgBytes, -1)
 		if !errors.Is(err, InvalidBlurPercentageError) {
 			t.Fatalf("expected error to be InvalidBlurPercentageError")
 		}
 
-		_, err = imagesService.Blur(img, 101)
+		_, err = imagesService.Blur(imgBytes, 101)
 		if !errors.Is(err, InvalidBlurPercentageError) {
 			t.Fatalf("expected error to be InvalidBlurPercentageError")
 		}
 	})
 
+	// Check './mock/processed/mock-image__blurred.jpg' to see the blurred image
 	t.Run("should return blurred image", func(t *testing.T) {
-		blurredImgBytes, err := imagesService.Blur(img, 0)
+		blurredImgBytes, err := imagesService.Blur(imgBytes, 0)
 		if err != nil {
 			t.Fatalf("expected no error, got %v", err)
 		}
@@ -56,8 +59,20 @@ func TestImagesService_Blur(t *testing.T) {
 			t.Fatalf("expected blurred image to have bytes")
 		}
 
-		// Uncomment the following line to save then compare the blurred image
-		//os.WriteFile("./mock/street_blurred.jpg", *blurredImgBytes, 0644)
+		blurredImgPointer, toImgErr := imagesService.bytesToImage(*blurredImgBytes)
+		if toImgErr != nil {
+			t.Fatalf("failed to convert bytes to image")
+		}
+
+		blurredImg := *blurredImgPointer
+		if blurredImg.Bounds().Dx() != img.Bounds().Dx() || blurredImg.Bounds().Dy() != img.Bounds().Dy() {
+			t.Fatalf("expected blurred image to have same dimensions as original image")
+		}
+
+		err = os.WriteFile("./mock/processed/mock-image__blurred.png", *blurredImgBytes, 0644)
+		if err != nil {
+			fmt.Println("failed to write blurred image")
+		}
 	})
 }
 
@@ -69,10 +84,11 @@ func TestImagesService_Crop(t *testing.T) {
 		}
 	})
 
+	// Check './mock/processed/mock-image__cropped.jpg' to see the cropped image
 	t.Run("should return cropped image", func(t *testing.T) {
 		x1, y1, width, height := 0, 0, 400, 200
 
-		croppedImgBytes, err := imagesService.Crop(img, [4]int{x1, y1, width, height})
+		croppedImgBytes, err := imagesService.Crop(imgBytes, [4]int{x1, y1, width, height})
 		if err != nil {
 			t.Fatalf("expected no error, got %v", err)
 		}
@@ -90,18 +106,28 @@ func TestImagesService_Crop(t *testing.T) {
 			t.Fatalf("expected cropped image to have width %d and height %d, got width %d and height %d", width, height, croppedImg.Bounds().Dx(), croppedImg.Bounds().Dy())
 		}
 
-		// Uncomment the following line to save then compare the cropped image
-		//os.WriteFile("./mock/street_cropped.jpg", *croppedImgBytes, 0644)
+		err = os.WriteFile("./mock/processed/mock-image__cropped.png", *croppedImgBytes, 0644)
+		if err != nil {
+			fmt.Println("failed to write cropped image")
+		}
 	})
 }
 
 func TestMain(m *testing.M) {
-	bytes, err := os.ReadFile("./mock/street.jpg")
+	bytes, err := os.ReadFile("./mock/mock-image.jpg")
 	if err != nil {
 		fmt.Println("failed to read image file")
 		os.Exit(1)
 	}
-	img = bytes
+	imgBytes = bytes
+
+	origImg, toImgErr := imagesService.bytesToImage(bytes)
+	if toImgErr != nil {
+		fmt.Println("failed to convert bytes to image")
+		os.Exit(1)
+	}
+
+	img = *origImg
 
 	os.Exit(m.Run())
 }
