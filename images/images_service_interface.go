b742ee966af60c6e9285e4d0f11873237e68e375
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-10-05 09:03:02 +0500

    update IIMagesService

--- images/images_service_interface.go
+++ images/images_service_interface.go
@@ -2,6 +2,7 @@ package images
 
 type IIMagesService interface {
 	CheckIsValidImage(image []byte) (bool, error)
+	// Blur percentage - 0 to 100, otherwise return InvalidBlurPercentageError
 	Blur(image []byte, percentage int32) ([]byte, error)
 	// Crop coords - [x1, y1, width, height]
 	Crop(image []byte, coords [4]int) ([]byte, error)
