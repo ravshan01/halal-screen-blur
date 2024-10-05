92f49c2d13c3d41a671f23fcdd60c84e442f5395
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-10-05 07:29:20 +0500

    update server.go

--- app/server.go
+++ app/server.go
@@ -3,6 +3,7 @@ package app
 import (
 	"context"
 	"fmt"
+	"halal-screen-blur/config"
 	"halal-screen-blur/proto"
 )
 
@@ -14,9 +15,40 @@ func NewServer() *Server {
 	return &Server{}
 }
 
-func (s *Server) BlurImages(ctx context.Context, req *proto.BlurImagesRequest) (*proto.BlurImageResponse, error) {
+func (s *Server) BlurImages(_ context.Context, req *proto.BlurImagesRequest) (*proto.BlurImageResponse, error) {
 	images := req.GetImages()
 	fmt.Println("images:", images)
 
+	res := s.checkHasImagesAndNotTooMany(images)
+	if res != nil {
+		return res, nil
+	}
+
 	return &proto.BlurImageResponse{}, nil
+}
+
+/*
+Return `BlurImagesResponse` with error if no images provided or too many images
+*/
+func (s *Server) checkHasImagesAndNotTooMany(images []*proto.ImageForBlur) *proto.BlurImageResponse {
+	if len(images) == 0 {
+		return &proto.BlurImageResponse{
+			Error: &proto.BlurError{
+				Code:    proto.BlurErrorCode_BadRequest,
+				Message: "no images provided",
+			},
+		}
+	}
+
+	cfg := config.GetConfig()
+	if len(images) > cfg.MaxImagesPerRequest {
+		return &proto.BlurImageResponse{
+			Error: &proto.BlurError{
+				Code:    proto.BlurErrorCode_BadRequest,
+				Message: "too many images",
+			},
+		}
+	}
+
+	return nil
 }
