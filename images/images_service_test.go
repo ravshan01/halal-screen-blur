160338c91dad9eae2a8a8f19964b15a0ee3e5e4d
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-10-05 11:23:12 +0500

    implemented TestImagesService_Blur

--- images/images_service_test.go
+++ images/images_service_test.go
@@ -1,6 +1,7 @@
 package images
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"testing"
@@ -22,7 +23,6 @@ func TestMain(m *testing.M) {
 }
 
 func TestImagesService_CheckIsValidImage(t *testing.T) {
-
 	t.Run("valid image", func(t *testing.T) {
 		valid := imagesService.CheckIsValidImage(img)
 		if !valid {
@@ -39,5 +39,36 @@ func TestImagesService_CheckIsValidImage(t *testing.T) {
 }
 
 func TestImagesService_Blur(t *testing.T) {
+	t.Run("should return InvalidImageError if image is invalid", func(t *testing.T) {
+		_, err := imagesService.Blur([]byte{}, 50)
+		if !errors.Is(err, InvalidImageError) {
+			t.Fatalf("expected error to be InvalidImageError")
+		}
+	})
 
+	t.Run("should return InvalidBlurPercentageError if percentage < 0 or > 100 ", func(t *testing.T) {
+		_, err := imagesService.Blur(img, -1)
+		if !errors.Is(err, InvalidBlurPercentageError) {
+			t.Fatalf("expected error to be InvalidBlurPercentageError")
+		}
+
+		_, err = imagesService.Blur(img, 101)
+		if !errors.Is(err, InvalidBlurPercentageError) {
+			t.Fatalf("expected error to be InvalidBlurPercentageError")
+		}
+	})
+
+	t.Run("should return blurred image", func(t *testing.T) {
+		blurredImg, err := imagesService.Blur(img, 50)
+		if err != nil {
+			t.Fatalf("expected no error, got %v", err)
+		}
+
+		if len(*blurredImg) == 0 {
+			t.Fatalf("expected blurred image to have bytes")
+		}
+
+		// Uncomment the following line to save the blurred image
+		//os.WriteFile("./mock/street_blurred.jpg", *blurredImg, 0644)
+	})
 }
