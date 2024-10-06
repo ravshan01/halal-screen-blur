ea3980baa356f6997302dbce40a456a65b9ef29c
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-10-06 10:13:50 +0500

    update proto-fetch-pull.cmd

--- scripts/proto-fetch-pull.cmd
+++ scripts/proto-fetch-pull.cmd
@@ -1,8 +1,15 @@
 @echo off
 
-REM Set default branch name
 set BRANCH_NAME=master
 if not "%1"=="" set BRANCH_NAME=%1
 
+REM git subtree pull ... is working only from the root of the repository
+REM so we need to change the directory to the root of the repository
+REM and then back to the original directory
+set DIR=%CD%
+cd ..
+
 git fetch halal-screen-proto %BRANCH_NAME%
 git subtree pull --prefix proto halal-screen-proto %BRANCH_NAME% --squash
+
+cd %DIR%
