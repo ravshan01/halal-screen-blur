bcfe27a9d55b5e5bd37418ffbe4ff12be5a6424f
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-10-06 11:27:11 +0500

    update images_service_test.go

--- images/images_service_test.go
+++ images/images_service_test.go
@@ -30,20 +30,13 @@ func TestImagesService_CheckIsValidImage(t *testing.T) {
 }
 
 func TestImagesService_Blur(t *testing.T) {
-	t.Run("should return InvalidImageError if image is invalid", func(t *testing.T) {
-		_, err := imagesService.Blur([]byte{}, 50)
-		if !errors.Is(err, InvalidImageError) {
-			t.Fatalf("expected error to be InvalidImageError")
-		}
-	})
-
 	t.Run("should return InvalidBlurPercentageError if percentage < 0 or > 100 ", func(t *testing.T) {
-		_, err := imagesService.Blur(imgBytes, -1)
+		_, err := imagesService.Blur(img, -1)
 		if !errors.Is(err, InvalidBlurPercentageError) {
 			t.Fatalf("expected error to be InvalidBlurPercentageError")
 		}
 
-		_, err = imagesService.Blur(imgBytes, 101)
+		_, err = imagesService.Blur(img, 101)
 		if !errors.Is(err, InvalidBlurPercentageError) {
 			t.Fatalf("expected error to be InvalidBlurPercentageError")
 		}
@@ -51,22 +44,19 @@ func TestImagesService_Blur(t *testing.T) {
 
 	// Check './mock/processed/mock-image__blurred.jpg' to see the blurred image
 	t.Run("should return blurred image", func(t *testing.T) {
-		blurredImgBytes, err := imagesService.Blur(imgBytes, 0)
+		blurredImgPointer, err := imagesService.Blur(img, 0)
 		if err != nil {
 			t.Fatalf("expected no error, got %v", err)
-		}
-		if len(*blurredImgBytes) == 0 {
-			t.Fatalf("expected blurred image to have bytes")
-		}
-
-		blurredImgPointer, toImgErr := imagesService.BytesToImage(*blurredImgBytes)
-		if toImgErr != nil {
-			t.Fatalf("failed to convert bytes to image")
 		}
 
 		blurredImg := *blurredImgPointer
 		if blurredImg.Bounds().Dx() != img.Bounds().Dx() || blurredImg.Bounds().Dy() != img.Bounds().Dy() {
 			t.Fatalf("expected blurred image to have same dimensions as original image")
+		}
+
+		blurredImgBytes, toBytesErr := imagesService.ImageToBytes(blurredImg)
+		if toBytesErr != nil {
+			t.Fatalf("failed to convert image to bytes")
 		}
 
 		err = os.WriteFile("./mock/processed/mock-image__blurred.png", *blurredImgBytes, 0644)
@@ -77,33 +67,24 @@ func TestImagesService_Blur(t *testing.T) {
 }
 
 func TestImagesService_Crop(t *testing.T) {
-	t.Run("should return InvalidImageError if image is invalid", func(t *testing.T) {
-		_, err := imagesService.Crop([]byte{}, [4]int{0, 0, 0, 0})
-		if !errors.Is(err, InvalidImageError) {
-			t.Fatalf("expected error to be InvalidImageError")
-		}
-	})
-
 	// Check './mock/processed/mock-image__cropped.jpg' to see the cropped image
 	t.Run("should return cropped image", func(t *testing.T) {
 		x1, y1, width, height := 0, 0, 400, 200
+		rect := image.Rect(x1, y1, x1+width, y1+height)
 
-		croppedImgBytes, err := imagesService.Crop(imgBytes, [4]int{x1, y1, width, height})
+		croppedImgPointer, err := imagesService.Crop(img, rect)
 		if err != nil {
 			t.Fatalf("expected no error, got %v", err)
-		}
-		if len(*croppedImgBytes) == 0 {
-			t.Fatalf("expected cropped image to have bytes")
-		}
-
-		croppedImgPointer, toImgErr := imagesService.BytesToImage(*croppedImgBytes)
-		if toImgErr != nil {
-			t.Fatalf("failed to convert bytes to image")
 		}
 
 		croppedImg := *croppedImgPointer
 		if croppedImg.Bounds().Dx() != width || croppedImg.Bounds().Dy() != height {
 			t.Fatalf("expected cropped image to have width %d and height %d, got width %d and height %d", width, height, croppedImg.Bounds().Dx(), croppedImg.Bounds().Dy())
+		}
+
+		croppedImgBytes, toBytesErr := imagesService.ImageToBytes(croppedImg)
+		if toBytesErr != nil {
+			t.Fatalf("failed to convert image to bytes")
 		}
 
 		err = os.WriteFile("./mock/processed/mock-image__cropped.png", *croppedImgBytes, 0644)
